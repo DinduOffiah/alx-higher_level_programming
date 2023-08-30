bd9253ce93618ddb775cca0d442a143e1b400846
DinduOffiah <99076158+DinduOffiah@users.noreply.github.com>
2023-08-30 14:59:47 +0100

    Exceptions

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     value = 0
     for i in range(list_length):
         try:
-            value = (my_list_1[i] / my_list_2[i])
+            value = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
             print('division by 0')
         except TypeError:
@@ -14,4 +15,4 @@ def list_division(my_list_1, my_list_2, list_length):
         finally:
             new_list.append(value)
             value = 0
-    return(new_list)
+    return new_list
