d94f679d0101ac3fbe6fe1560284f047d2200790
DinduOffiah <99076158+DinduOffiah@users.noreply.github.com>
2023-08-30 15:51:36 +0100

    Classes

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,28 +1,23 @@
 #!/usr/bin/python3
-""" Class Square that defines a square """
-
-
 class Square:
-    """ Class Square """
     def __init__(self, size=0):
-        """ Constructor """
-        self.size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
 
     @property
     def size(self):
-        """ Getter """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """ Setter """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
-
-    def area(self):
-        """ Calculatre square area """
-        return(self.__size * self.__size)
+        self.__size = value
