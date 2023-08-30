446757eda61d60dbfe8643eebcdf72feb8679ce2
DinduOffiah <99076158+DinduOffiah@users.noreply.github.com>
2023-08-30 15:48:23 +0100

    Classes

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
-""" Class Square that defines a square """
 
+"""
+Class Square that defines a square
+"""
 
 class Square:
-    """ Class Square """
+    """
+    Class Square
+    """
+
     def __init__(self, size=0):
-        """ Constructor """
+        """
+        Constructor
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -14,5 +21,7 @@ class Square:
             self.__size = size
 
     def area(self):
-        """ Calculatre square area """
-        return(self.__size * self.__size)
+        """
+        Calculate square area
+        """
+        return self.__size * self.__size
